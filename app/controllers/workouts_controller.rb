f40beba8e195100f0384e56c722a9a4fa71f5831
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-09-06 23:52:02 -0500

    views for workout form, new, show, index, added some actions to workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,42 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :edit, :update, :destroy] 
+
   def index
     @workouts = Workout.all
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def show
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save 
+      redirect_to @workout
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
+  def destroy
+  end
+
+  private
+  def workout_params
+    params.require(:workout).permit(:date, :workout, :mood, :length, :comments)
+
+  end
+
+  def find_workout
+    @workout = Workout.find(params[:id])
   end
 end
